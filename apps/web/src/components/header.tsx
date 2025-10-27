f1d85861350d449042ac27261278fa90fd0b0096
guisandroni <gui.sandroni@gmail.com>
2025-10-27 04:49:54 -0300

    chore: update layout structure, global styles, and route configuration

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,26 +1,26 @@
-import { Link } from "@tanstack/react-router";
-import { ModeToggle } from "./mode-toggle";
+import { Link } from '@tanstack/react-router';
+import { ModeToggle } from './mode-toggle';
+import { AnimatedThemeToggler } from './ui/animated-theme-toggler';
 
 export default function Header() {
-	const links = [{ to: "/", label: "Home" }] as const;
+  const links = [{ to: '/', label: 'Home' }] as const;
 
-	return (
-		<div>
-			<div className="flex flex-row items-center justify-between px-2 py-1">
-				<nav className="flex gap-4 text-lg">
-					{links.map(({ to, label }) => {
-						return (
-							<Link key={to} to={to}>
-								{label}
-							</Link>
-						);
-					})}
-				</nav>
-				<div className="flex items-center gap-2">
-					<ModeToggle />
-				</div>
-			</div>
-			<hr />
-		</div>
-	);
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-between  ">
+        <nav className="flex-row items-center justify-center text-md text-gray-400 mt-6 italic tracking-wider flex gap-4 font-semibold mb-2">
+          <Link to="/">
+            <span>guisandroni</span>
+          </Link>
+        </nav>
+        <div className="flex-row items-center justify-center text-md text-gray-400 mt-6 italic tracking-wider flex gap-8 font-semibold mb-2">
+          {/*<AnimatedThemeToggler />*/}
+          <Link to="/projects">Projects</Link>
+          <Link to="/bio">About</Link>
+          <Link to="/blog">Posts</Link>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
 }
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,60 +1,67 @@
-import Header from "@/components/header";
-import Loader from "@/components/loader";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
+import Header from '@/components/header';
+import Loader from '@/components/loader';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 import {
-	HeadContent,
-	Outlet,
-	createRootRouteWithContext,
-	useRouterState,
-} from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import "../index.css";
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+  useRouter,
+  useRouterState
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import '../index.css';
 
 export interface RouterAppContext {}
 
 export const Route = createRootRouteWithContext<RouterAppContext>()({
-	component: RootComponent,
-	head: () => ({
-		meta: [
-			{
-				title: "portfolio-blog",
-			},
-			{
-				name: "description",
-				content: "portfolio-blog is a web application",
-			},
-		],
-		links: [
-			{
-				rel: "icon",
-				href: "/favicon.ico",
-			},
-		],
-	}),
+  component: RootComponent,
+  head: () => ({
+    meta: [
+      {
+        title: 'portfolio-blog'
+      },
+      {
+        name: 'description',
+        content: 'portfolio-blog is a web application'
+      }
+    ],
+    links: [
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  })
 });
 
 function RootComponent() {
-	const isFetching = useRouterState({
-		select: (s) => s.isLoading,
-	});
+  const isFetching = useRouterState({
+    select: (s) => s.isLoading
+  });
 
-	return (
-		<>
-			<HeadContent />
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="dark"
-				disableTransitionOnChange
-				storageKey="vite-ui-theme"
-			>
-				<div className="grid grid-rows-[auto_1fr] h-svh">
-					<Header />
-					{isFetching ? <Loader /> : <Outlet />}
-				</div>
-				<Toaster richColors />
-			</ThemeProvider>
-			<TanStackRouterDevtools position="bottom-left" />
-		</>
-	);
+  const router = useRouter();
+  const currentRoute = router.state.location.pathname;
+  const isHome = currentRoute === '/';
+
+  return (
+    <>
+      <HeadContent />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+        storageKey="vite-ui-theme"
+      >
+        <div className="min-h-screen flex justify-center items-center  ">
+          <div className="grid grid-rows-[auto_1fr] h-svh  max-w-7xl w-full">
+            {!isHome && <Header />}
+            {isFetching ? <Loader /> : <Outlet />}
+          </div>
+        </div>
+        <Toaster richColors />
+      </ThemeProvider>
+      <TanStackRouterDevtools position="bottom-left" />
+    </>
+  );
 }
